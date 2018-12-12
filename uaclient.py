6c523a504a1efd9cf8970f696fd66999d7c0a3a0
Felicidad Abad Quintanilla <fabadq@gamma07.aulas.gsyc.urjc.es>
2018-12-12 10:58:35 +0100

    UAClient casi acabado

--- uaclient.py
+++ uaclient.py
@@ -8,6 +8,7 @@ import socket
 import sys
 from datetime import datetime, timedelta
 import os.path
+import os
 import xml.etree.ElementTree as ET
 
 class UAClient:
@@ -24,87 +25,104 @@ class UAClient:
         return self.xml_dicc
 
     def building_sip(self):
-    # SACO LO QUE NECESITO, FALTA MONTAR SIP
         self.config()
 
         username = str(self.xml_dicc['account']['username'])
         rtp_port = self.xml_dicc['rtpaudio']['puerto']
-        proxy_port = self.xml_dicc['regproxy']['puerto']
-        proxy_ip = self.xml_dicc['regproxy']['ip']
-        server_ip = self.xml_dicc['uaserver']['ip']
-        server_port = self.xml_dicc['uaserver']['puerto']
-        print("----------")
-        print(proxy_port + " " + username)
+        my_ip = self.xml_dicc['uaserver']['ip']
+        my_port = self.xml_dicc['uaserver']['puerto']
      
-        if sys.argv[3] == 'REGISTER':
-            expires = sys.argv[4]
-            message = 'REGISTER sip:' + username + ':' + server_port + ' SIP/2.0\r\n'
-            message += 'Expires: ' + expires
+        method = sys.argv[2]
+        option = sys.argv[3]
+        
+        if method == 'REGISTER':
+            self.message = method + ' sip:' + username + ':' + my_port + ' SIP/2.0\r\n'
+            self.message += 'Expires: ' + option
+        elif method == 'INVITE':
+            self.message = method + ' sip:' + option + ' SIP/2.0\r\n'
+            self.message += 'Content-Type: application/sdp\r\n' + '\r\n'
+            self.message += 'v=0\r\n' + 'o=' + username + ' ' + my_ip + '\r\n'
+            self.message += 's=misesion\r\n' + 't=0\r\n' + 'm=audio ' + rtp_port + ' RTP\r\n'
+        elif method == 'BYE':
+            self.message = method + ' sip:' + option + ' SIP/2.0\r\n'
+        else:
+            self.message = method + ' sip:' + option + ' SIP/2.0\r\n'
+            print("Petición inválida")
         print(self.message)
         return self.message
 
-cliente = UAClient()
-dicc = cliente.config()
-prueba = cliente.building_sip()
-print(prueba)
+    def writing_log(self):
+        self.building_sip()
 
-proxy_ip = dicc['regproxy']['ip']
-proxy_port = int(dicc['regproxy']['puerto'])
+        format_date = '%Y%m%d%H%M%S'
+        time = datetime.now()
+        date = time.strftime(format_date)
 
-if len(sys.argv) != 4:
-    sys.exit("Usage: python uaclient.py config metodo opcion")
+        file_rute = self.xml_dicc['log']['path']
 
-method = sys.argv[2]
-print(method)
+        username = str(self.xml_dicc['account']['username'])
+        rtp_port = self.xml_dicc['rtpaudio']['puerto']
+        proxy_ip = self.xml_dicc['regproxy']['ip']
+        proxy_port = self.xml_dicc['regproxy']['puerto']
+        my_port = self.xml_dicc['uaserver']['puerto']
+
+        if os.path.exists(file_rute):
+            file = open(file_rute, 'a')
+        else:
+            file = open(file_rute, 'w')
+
+        EVENT = date + " Starting..." + "\r"
+        file.write(EVENT)
+        print(EVENT)
+        if sip_message != '\r\n':
+            if method == 'REGISTER':
+                EVENT = date + " Sent to " + proxy_ip + ":" + proxy_port + ": " + method
+                EVENT += " sip:" + username + ":" + my_port + " SIP/2.0\r"
+                file.write(EVENT)
+                print(EVENT)
+            elif method == 'INVITE':
+                EVENT = date + " Sent to " + proxy_ip + ":" + proxy_port + ":" + method
+                EVENT += " " + option + " SIP/2.0\r"
+                file.write(EVENT)
+            elif method == 'BYE':
+                EVENT = date + " Sent to" + proxy_ip + ":" + proxy_port + ":" + method + "\r"
+                file.write(EVENT)
+        # TERMINAR BUCLE CON LO RECIBIDO
+
+if __name__ == "__main__":
+    try:
+        method = sys.argv[2]
+        option = sys.argv[3]
+    except (IndexError, ValueError, NameError):
+        sys.exit("Usage: python uaclient.py config metodo opcion")
+
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python uaclient.py config metodo opcion")
+
+    client = UAClient()
+    dicc = client.config()
+    sip_message = client.building_sip()
+    log = client.writing_log()
+
+    proxy_ip = dicc['regproxy']['ip']
+    proxy_port = int(dicc['regproxy']['puerto'])
 
 #FALTA CAPTURAR ERROR DE NO PUERTO CONECTADO
-try:
-    if method == 'REGISTER':
-        expires = sys.argv[3]
-        print(expires)
-    elif method =='INVITE':
-        invited = sys.argv[3]
-    else:
-        sys.exit("Método inválido")
-except (IndexError, ValueError, NameError):
-    sys.exit("Usage: python uaclient.py config metodo opcion")
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((proxy_ip, proxy_port))
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((proxy_ip, proxy_port))
 
-    message = 'ay'
-    print("Enviando: " + message)
-    my_socket.send(bytes(message, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    format_date = '%Y%m%d%H%M%S'
-    time = datetime.now()
-    date = time.strftime(format_date)
-    print(date)
+        print("Enviando: " + sip_message)
+        my_socket.send(bytes(sip_message, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
 
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
+        print('Recibido -- ', data.decode('utf-8'))
+        print("Terminando socket...")
 
     if data.decode('utf-8') != '':
         received = data.decode('utf-8')
         print(received)
-        if 'Trying' in received and 'Ringing' in received and 'OK' in received:
-            message2 = 'ACK' + " sip:" + receiver + "@" + IP + " SIP/2.0"
-            print("Enviando " + message2)
-            my_socket.send(bytes(message2, 'utf-8') + b'\r\n')
-
-    if os.path.exists('log.txt'):
-        file = open('log.txt', 'w')
-        
-        EVENT = date + " Starting..." + "\r\n"
-        file.write(EVENT)
-        print(EVENT)
-        if message != '\r\n':
-            EVENT = date + " Sent to " + "CAMBIAR" + ":" + "CAMBIAR" + ": " + method
-            EVENT += " sip:" + "CAMBIAR"
-            file.write(EVENT)
-            print(EVENT)
-        # TERMINAR BUCLE
 
 print("Fin.")
