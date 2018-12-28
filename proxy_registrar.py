de7b14c571bd00e2bb411cc1b9ffa7a87ae9e175
Felicidad Abad Quintanilla <fabadq@zeta.aulas.gsyc.urjc.es>
2018-12-29 00:40:26 +0100

    Busca en el diccionario para reenviar si tengo INVITE o BYE

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -107,10 +107,18 @@ class EchoHandler(socketserver.DatagramRequestHandler, Proxy):
                 self.wfile.write(bytes(response, 'utf-8'))
                 sent_event = ' Sent to ' + IP + ':' + str(port) + ': ' + response1line
 
-        if request[0] == 'INVITE' or request[0] == 'BYE' or request[0] == 'ACK':
+        if request[0] == 'INVITE' or request[0] == 'BYE':
             # FALTA BUSCAR EN DICCIONARIO PARA REENVIAR EL MENSAJE
             IP = self.client_address[0]
             port = str(self.client_address[1])
+            address = request[1][request[1].find(':')+1:]
+            print(address)
+            invited_ip = self.client_dicc[address][0]
+            invited_port = self.client_dicc[address][1]
+            sent_event = ' Sent to '
+            print(invited_ip, str(invited_port))
+        if request[0] == 'ACK':
+            print("falta")
 
 
         recv_event = ' Received from ' + IP + ':' + port + ': ' + prueba1
