5207ecd39d3a340b791707429f666ba7c92e93d5
Felicidad Abad Quintanilla <fabadq@beta.aulas.gsyc.urjc.es>
2018-12-28 01:47:27 +0100

    log proxy de recibidos apañado, falta mucho

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import os.path
 import os
 import xml.etree.ElementTree as ET
+import json
 
 class Proxy:
     client_dicc = {}
@@ -18,17 +19,25 @@ class Proxy:
         root = tree.getroot()
         for branch in root:
             self.xml_dicc[str(branch.tag)] = branch.attrib
-        print(self.xml_dicc)
         return self.xml_dicc
 
-    def registeredfile(self):
-    # CREO ARCHIVOS DE CLIENTES REGISTRADOS
-        self.config()
-        file_rute = self.xml_dicc['database']['path']
-        if os.path.exists(file_rute):
-            file = open(file_rute, 'a')
+    def register2json(self):
+        """
+        Creates json file
+        """
+        with open('registered.json', 'w') as json_file:
+            json.dump(self.client_dicc, json_file)
+
+    def json2registered(self):
+        """
+        Checks if there's a json file,
+        if not, it creates it
+        """
+        if os.path.exists('registered.json'):
+            with open('registered.json', 'w') as json_file:
+                json.dump(self.client_dicc, json_file)
         else:
-            file = open(file_rute, 'w')
+            self.register2json()
 
     def passwdfile(self):
         self.config()
@@ -48,11 +57,15 @@ class Proxy:
             file = open(file_rute, 'w')
         file.write(event)
 
+
 class EchoHandler(socketserver.DatagramRequestHandler, Proxy):
     """
     Echo server class
     """
     def handle(self):
+
+        lines = []
+
         format_date = '%Y%m%d%H%M%S'
         time = datetime.now()
         date = time.strftime(format_date)
@@ -64,27 +77,39 @@ class EchoHandler(socketserver.DatagramRequestHandler, Proxy):
             if line.decode('utf-8') == '\r\n' or not line:
                 continue
             else:
-                request = line.decode('utf-8').split(" ")
                 received_message = line.decode('utf-8')
+                lines.append(received_message) 
 
-            if request[0] == 'REGISTER' and request [2] == 'SIP/2.0\r\n':
-                self.registeredfile()
-                address = request[1][request[1].find(':')+1:request[1].rfind(':')]
-                IP = self.client_address[0]
-                port = self.client_address[1]
+        prueba = ''.join(lines)
+        prueba1 = prueba.replace('\r\n', ' ')
+        request = prueba1.split(' ')
+        print(prueba1)
+        print(request)
 
-                event = date + ' Received from ' + IP + ':' + str(port) + ' ' + received_message
-                self.logfile(event)
+        if request[0] == 'REGISTER' and request[2] == 'SIP/2.0':
+            address = request[1][request[1].find(':')+1:request[1].rfind(':')]
+            IP = self.client_address[0]
+            port = request[1][request[1].rfind(':')+1:]
 
-                user_data = address + " " + IP + " " + str(port)
-                print(user_data)
+            event = date + ' Received from ' + IP + ':' + str(port) + ': ' + received_message
+            self.logfile(event)
+            print(event)
 
-            elif request[0] == 'Expires:':
-                print('meh')
+            user_data = address + " " + IP + " " + str(port)
+            print(user_data)      
 
+        if request[0] == 'INVITE' or request[0] == 'BYE' or request[0] == 'ACK':
+            IP = self.client_address[0]
+            port = str(self.client_address[1])
+            event = date + ' Received from ' + IP + ':' + port + ': ' + received_message
+            print(event)
+            self.logfile(event)
+
+        self.json2registered()
+        print(self.client_dicc)
 
 if __name__ == "__main__":
-    print("prueba")
+    print("-----------------------------------")
 
     try:
         config = sys.argv[1]
